695effa0adc15cd8ad3ffa5f2471264af5f28cdb
Assel Kebede <asselmeseret@gmail.com>
2022-09-04 05:38:37 -0700

    beautiful code that passes the pycodestyle checks

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 This module contains the the entry point of the command interpreter
 for the AirBnB project.
 """
+import re
 import cmd
 from models.base_model import BaseModel
 from models import storage
@@ -16,9 +17,21 @@ from models.state import State
 
 def build_args(line):
     """
-    This function builds the custome command line arguments.
+    This function builds custom command line arguments for further
+    processing.
     """
     return line.split()
+
+
+def make_str_without_quotes(string):
+    """
+    This function reconstructes the str without quote
+    """
+    ret = ""
+    for ch in string:
+        if ch != "\"":
+            ret += ch
+    return ret
 
 
 class HBNBCommand(cmd.Cmd):
@@ -168,6 +181,60 @@ class HBNBCommand(cmd.Cmd):
                 else:
                     obj.__dict__[k] = v
         storage.save()
+
+    def do_instance_counter(self, line):
+        """
+        Counts the number of instances of a class
+        """
+        number_of_instances = 0
+        for instance in storage.all().values():
+            if build_args(line)[0] == type(instance).__name__:
+                number_of_instances += 1
+
+        print(number_of_instances)
+
+    def default(self, line):
+        """
+        Method called on an input line when the command prefix is not
+        recognized.
+        """
+        cmd_dict = {
+                "all": self.do_all,
+                "count": self.do_instance_counter,
+                "show": self.do_show,
+                "destroy": self.do_destroy,
+                "update": self.do_update,
+                }
+
+        pattern = re.compile(r"^(\w+)\.(\w+)\((.*)\)")
+        matches = re.findall(pattern, line)
+        if (not matches or len(matches[0]) < 2 or
+                matches[0][0] not in HBNBCommand.classes or
+                matches[0][1] not in cmd_dict.keys()):
+            super().default(line)
+            return
+
+        if matches[0][1] == "all" or matches[0][1] == "count":
+            cmd_dict[matches[0][1]](matches[0][0])
+        elif matches[0][1] == "destroy" or matches[0][1] == "show":
+            obj_id = matches[0][2][1:-1]
+            cmd_dict[matches[0][1]](matches[0][0] + " " + obj_id)
+        elif matches[0][1] == "update":
+            pattern = re.compile(r"\"(.+?)\", (.+)")
+            sub_match = re.match(pattern, matches[0][2]).groups()
+            if sub_match[1][0] != '{':
+                key_value = sub_match[1].split(", ")
+                key = make_str_without_quotes(key_value[0])
+                val = make_str_without_quotes(key_value[1])
+                param = "{} {} {} {}".format(
+                        matches[0][0], sub_match[0], key, val)
+                cmd_dict[matches[0][1]](param)
+            else:
+                dict_repr = eval(sub_match[1])
+                for k, v in dict_repr.items():
+                    param = "{} {} {} {}".format(
+                            matches[0][0], sub_match[0], k, v)
+                    cmd_dict[matches[0][1]](param)
 
 
 if __name__ == '__main__':
